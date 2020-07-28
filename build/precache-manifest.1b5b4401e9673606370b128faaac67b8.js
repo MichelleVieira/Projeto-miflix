50015e18cd7df4f99d9d523f3b70778283415262
michelle.dmessias <michelle.dmessias@grupofleury.com.br>
2020-07-28 09:22:02 -0300

    Setup inicial do projeto

---
+++ build/precache-manifest.1b5b4401e9673606370b128faaac67b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eebbf0ea642f724400ea379fe6ba7b57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45d30e381886f26b8184",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "74f1250b190f6fc741b6",
+    "url": "/static/js/2.a832aea7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a832aea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45d30e381886f26b8184",
+    "url": "/static/js/main.f71acd26.chunk.js"
+  },
+  {
+    "revision": "f5fed4fb47583d9dd73b",
+    "url": "/static/js/runtime-main.2891f158.js"
+  }
+]);
---
+++ build/static/js/main.f71acd26.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmiflix=this.webpackJsonpmiflix||[]).push([[0],[,,,function(e,n,t){e.exports=t(10)},,,,,function(e,n,t){},function(e,n,t){},function(e,n,t){"use strict";t.r(n);var a=t(0),c=t.n(a),i=t(2),r=t.n(i);t(8),t(9);var o=function(){return c.a.createElement("div",{className:"App"})};r.a.render(c.a.createElement(c.a.StrictMode,null,c.a.createElement(o,null)),document.getElementById("root"))}],[[3,1,2]]]);
+//# sourceMappingURL=main.f71acd26.chunk.js.map
